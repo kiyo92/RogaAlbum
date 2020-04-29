9b18453d8116e1c42fdd256ad129003ec4b3d7d4
João Marcus <joao_marcus92@hotmail.com>
2020-04-29 16:05:32 -0300

    Initial Commit

---
+++ RogaAlbum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RogaAlbum
+//
+//  Created by Joao Marcus Dionisio Araujo on 29/04/20.
+//  Copyright © 2020 Joao Marcus Dionisio Araujo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RogaAlbumTests/RogaAlbumTests.swift
@@ -0,0 +1,34 @@
+//
+//  RogaAlbumTests.swift
+//  RogaAlbumTests
+//
+//  Created by Joao Marcus Dionisio Araujo on 29/04/20.
+//  Copyright © 2020 Joao Marcus Dionisio Araujo. All rights reserved.
+//
+
+import XCTest
+@testable import RogaAlbum
+
+class RogaAlbumTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
